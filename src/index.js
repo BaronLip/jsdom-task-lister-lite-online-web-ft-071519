2e9c8a2cb61437dd739d825a378070a53d3bc564
BaronLip <baronlip@gmail.com>
2019-10-17 16:36:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    
+    const createTaskForm = document.getElementById('create-task-form');
+    
+    // on submit event, perform these steps,
+    createTaskForm.addEventListener("submit", function(e){
+        // const createTaskButton = document.getElementById('create-task-button');
+        const newTaskDescription = document.getElementById('new-task-description');
+        const tasks = document.getElementById('tasks');
+        
+        
+        // prevent default action of submiting
+        e.preventDefault();
+        
+        // create an HTML element, 
+        const newTask = document.createElement("li");
+        
+        // place the new li into "tasks" list,
+        tasks.appendChild(newTask)
+        
+        // add the content into the li.
+        newTask.innerText = newTaskDescription.value;     
+    }); 
 });
